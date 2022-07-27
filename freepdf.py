36101e5edda6587e2b659281f7bedb737567c00e
codeinchicago <vancewcole@gmail.com>
2022-07-27 15:11:49 -0500

    Worked through examples

--- freepdf.py
+++ freepdf.py
@@ -1,3 +1,7 @@
+#TODO:
+#Need to ask if overriding a file.
+#GUI w/ Tkinter
+
 #Initial demonstration
 
 # import PyPDF2 as pdf
@@ -17,29 +21,63 @@
 
 
 #Merging PDFs
-import PyPDF2 as pdf
 
-pdf1File = open('meetingminutes.pdf', 'rb')
-pdf2File = open('meetingminutes2.pdf', 'rb')
+# import PyPDF2 as pdf
 
-pdf1Reader = pdf.PdfFileReader(pdf1File)
-pdf2Reader = pdf.PdfFileReader(pdf2File)
+# pdf1File = open('meetingminutes.pdf', 'rb')
+# pdf2File = open('meetingminutes2.pdf', 'rb')
 
-pdfWriter = pdf.PdfFileWriter()
+# pdf1Reader = pdf.PdfFileReader(pdf1File)
+# pdf2Reader = pdf.PdfFileReader(pdf2File)
 
-for pageNum in range(pdf1Reader.numPages):
-    pageObj = pdf1Reader.getPage(pageNum)
-    pdfWriter.addPage(pageObj)
+# pdfWriter = pdf.PdfFileWriter()
 
-for pageNum in range(pdf2Reader.numPages):
-    pageObj = pdf2Reader.getPage(pageNum)
-    pdfWriter.addPage(pageObj)
+# for pageNum in range(pdf1Reader.numPages):
+#     pageObj = pdf1Reader.getPage(pageNum)
+#     pdfWriter.addPage(pageObj)
+
+# for pageNum in range(pdf2Reader.numPages):
+#     pageObj = pdf2Reader.getPage(pageNum)
+#     pdfWriter.addPage(pageObj)
 
 
-pdfOutputFile = open('combo.pdf', 'wb')
+# pdfOutputFile = open('combo.pdf', 'wb')
 
-pdfWriter.write(pdfOutputFile)
-pdfOutputFile.close()
-pdf1File.close()
-pdf2File.close()
+# pdfWriter.write(pdfOutputFile)
+# pdfOutputFile.close()
+# pdf1File.close()
+# pdf2File.close()
 
+
+#Rotating Pages
+# import PyPDF2 as pp
+# minutesFile = open('meetingminutes.pdf', 'rb')
+# pdfReader = pp.PdfFileReader(minutesFile)
+# page = pdfReader.getPage(0)
+# page.rotateClockwise(90)
+# pdfWriter = pp.PdfFileWriter()
+# pdfWriter.addPage(page)
+# resultPdfFile=open('rotatedPage.pdf','wb')
+# pdfWriter.write(resultPdfFile)
+# resultPdfFile.close()
+# minutesFile.close()
+
+
+#Overlaying Pages
+# import PyPDF2 as pp
+# minutesFile = open('meetingminutes.pdf', 'rb')
+# pdfReader = pp.PdfFileReader(minutesFile)
+# minutesFirstPage = pdfReader.getPage(0)
+# pdfWatermarkReader = pp.PdfFileReader(open('watermark.pdf', 'rb'))
+# minutesFirstPage.mergePage(pdfWatermarkReader.getPage(0))
+# pdfWriter = pp.PdfFileWriter()
+# pdfWriter.addPage(minutesFirstPage)
+
+# for pageNum in range(1,pdfReader.numPages):
+#     pageObj = pdfReader.getPage(pageNum)
+#     pdfWriter.addPage(pageObj)
+
+# resultPdfFile = open('watermarkedCover.pdf', 'wb')
+# pdfWriter.write(resultPdfFile)
+# minutesFile.close()
+# resultPdfFile.close()
